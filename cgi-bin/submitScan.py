38e4d3b09f3b7524e66f2b9ab89b2d5c370c7e62
BenfieldNick <ndb@pdx.edu>
2022-01-25 22:00:43 -0800

    Added success and fail page to room scans

--- cgi-bin/submitScan.py
+++ cgi-bin/submitScan.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import cgi
 import requests
-
 form = cgi.FieldStorage()
 email = form.getvalue("email")
 room_id = form.getvalue("roomNumber")
-
 json_data = {"scan": {"email": email, "room_id": room_id}}
 req = requests.post("https://contact-api-dev-3sujih4x4a-uc.a.run.app/record_data", json=json_data)
-print("Content-type: text/html\n")
-print(req.status_code)
+if req.status_code == requests.codes.ok+1:
+    url = "http://34.134.138.81/scanSuccess.html"
+    new_page = requests.post(url)
+    print("Content-type: text/html\n")
+    print(req.status_code)
+    print(new_page.text)
+else:
+    url = "http://34.134.138.81/scanFail.html"
+    new_page = requests.post(url)
+    print("Content-type: text/html\n")
+    print(req.status_code)
+    print(req.text)
+    print(new_page.text)
