de5708bbc26397e402dbecc741f424f3d817d720
BenfieldNick <ndb@pdx.edu>
2022-02-24 21:50:36 -0800

    Finishing Admin Page

--- cgi-bin/admin.py
+++ cgi-bin/admin.py
@@ -41,61 +41,148 @@ if(query == "records"):
         X Coord: """ + str(x) + """<br>
         Y Coord: """ + str(y) + """
         </p><br>"""
-elif(query == "students"):
-    req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/stats/?stat_type=" + query)
+elif(query == "breakouts"):
+    email = form.getvalue("email")
+    date = form.getvalue("date")
+    req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/breakout?email=" + email + "&date=" + date)
     print("Content-type: text/html\n")
     data = req.json()
     page = page + """<fieldset>
-    <legend>""" + query.upper() + """</legend>"""
-    for item in data:
-        email = str(item[0])
-        name = str(item[1])
-        studentid = str(item[2])
-        page = page + """<p class="results">
-        Email: """ + email + """<br>
-        Name: """ + name + """<br>
-        Student ID: """ + studentid + """ 
-        </p><br>"""
+    <legend>Breakouts</legend>"""
+    #print(data)
+    if(req.status_code == 400):
+        page = page + """<h4 class="successText">
+        """ + str(data["detail"]) + """</h4>"""
+    else:
+        page = page + """<h4 class="successText">
+        Students that may have been in contact with:<br> """ + email + """</h4>"""
+        if(data):
+            for item in data:
+                userid = str(item[0])
+                email = str(item[1])
+                date = str(item[2])
+                room = str(item[3])
+                xcoord = str(item[4])
+                ycoord = str(item[5])
+                page = page + """<p class="results">
+                User Id: """ + userid + """<br>
+                Email: """ + email + """<br>
+                Date: """ + date + """<br>
+                Room: """ + room + """<br>
+                X Coord: """ + xcoord + """<br>
+                Y Coord: """ + ycoord + """<br>
+                </p><br>"""
+        else:
+            page = page + """<h4 class="successText">
+            """ + str(data) + """ </h4> """
 elif(query == "buildings"):
+    print("Content-type: text/html\n")
+    page = page + """<fieldset>
+    <legend>""" + query.upper() + """</legend>"""
     req = ''
     if(form.getvalue("building")):
         b_id = form.getvalue("building")
         req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/buildings?building_id=" + b_id)
+        data = req.json()
+        #print(data)
+        building = str(data["Building"])
+        rooms = str(data["Number_of_Rooms"])
+        numscans = str(data["Number_of_Scans"])
+        unique = str(data["Unique_Students"])
+        scans = str(data["Scans_per_Day"])
+        page = page + """<p class="results">
+        Building: """ + building + """<br>
+        Number of Rooms: """ + rooms + """<br>
+        Number of Scans: """ + numscans + """<br>
+        Number of Unique Students: """ + unique + """<br>
+        Scans per Day: """ + scans + """<br>
+        </p><br>"""
     else:
         req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/buildings")
+        data = req.json()
+        if(req):
+            for item in data:
+                for att, val in item.items():
+                    #print("Item1: ", att)
+                    #print("Item2: ", val)
+                    page = page + """<p class="results">
+                    """ + str(att) + """: """ + str(val) + """\n"""
+                page = page + """</p><br>"""
+        else:
+            page = page + """<p class="results">
+            """ + req.text + """ or can't find building id
+            </p><br>"""
+elif(query == "rooms"):
     print("Content-type: text/html\n")
-    data = req.json()
     page = page + """<fieldset>
     <legend>""" + query.upper() + """</legend>"""
-    if(req):
-        for item in data:
-            for att, val in item.items():
-                #print("Item1: ", att)
-                #print("Item2: ", val)
-                page = page + """<p class="results">
-                """ + str(att) + """: """ + str(val) + """\n"""
-            page = page + """</p><br>"""
-elif(query == "rooms"):
     req = ''
-    if(form.getvalue("roomd")):
+    if(form.getvalue("roomid")):
         r_id = form.getvalue("roomid")
         req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/rooms?room_id=" + r_id)
-    else:
-        req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/rooms")
-    print("Content-type: text/html\n")
-    page = page + """<fieldset>
-    <legend>""" + query.upper() + """</legend>"""
-    for item in data:
-        roomid = str(item[0])
-        cap = str(item[1])
-        name = str(item[2])
-        ratio = str(item[3])
+        data = req.json()
+        #print(data)
+        roomid = str(data["Room"])
+        cap = str(data["Capacity"])
+        name = str(data["Building_Name"])
+        ratio = str(data["Aspect_Ratio"])
+        unique = str(data["Unique_Students"])
+        scans = str(data["Scans_per_Day"])
         page = page + """<p class="results">
         Room ID: """ + roomid + """<br>
         Capacity: """ + cap + """<br>
         Building Name: """ + name + """<br>
         Room Ratio: """ + ratio + """<br>
+        Number of Unique Students: """ + unique + """<br>
+        Scans per Day: """ + scans + """<br>
         </p><br>"""
+    else:
+        req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/rooms")
+        data = req.json()
+        if(req):
+            for item in data:
+                for att, val in item.items():
+                    #print("Item1: ", att)
+                    #print("Item2: ", val)
+                    page = page + """<p class="results">
+                    """ + str(att) + """: """ + str(val) + """\n"""
+                page = page + """</p><br>"""
+        else:
+            page = page + """<p class="results">
+            """ + req.text + """ or can't find room id
+            </p><br>"""
+elif(query == "userRecords"):
+    print("Content-type: text/html\n")
+    page = page + """<fieldset>
+    <legend>User Records</legend>"""
+    email = form.getvalue("email")
+    limit = form.getvalue("limit")
+    req = requests.get("https://contact-api-internal-dev-3sujih4x4a-uc.a.run.app/user_records?email=" + email + "&limit=" + limit)
+    data = req.json()
+    #print(data)
+    if(req.status_code == 400):
+        page = page + """<h4 class="successText">
+        """ + str(data["detail"]) + """</h4>"""
+    else:
+        if(data):
+            for item in data:
+                userid = str(item[0])
+                email = str(item[1])
+                date = str(item[2])
+                room = str(item[3])
+                xcoord = str(item[4])
+                ycoord = str(item[5])
+                page = page + """<p class="results">
+                User Id: """ + userid + """<br>
+                Email: """ + email + """<br>
+                Date: """ + date + """<br>
+                Room: """ + room + """<br>
+                X Coord: """ + xcoord + """<br>
+                Y Coord: """ + ycoord + """<br>
+                </p><br>"""
+        else:
+            page = page + """<h4 class="successText">
+            """ + str(data) + """ </h4> """
 page = page + """</fieldset>
 </form>
 </body>"""
